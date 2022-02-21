238d2ea4d377261c638b7ab4c1a7d6b7eaf30cec
florianj77 <florian.joerg@aon.at>
2022-02-21 12:51:48 +0100

    deleted old files

--- protex/charmm_ff/old_transfer_skripts/run.sh
+++
@@ -1,9 +0,0 @@
-for i in `seq 1 5000`
-do
-    echo "Run $i"
-    c42a1 index:$i <npt.inp >out/npt_$i.out
-
-    echo "Proton transfer ..."
-    python3 analysis/proton_transfer.py im1h_oac_500_$i.dcd im1h_oac_500_$i.psf
-    mv current_charges.str str/current_charges_$i.str
-done
