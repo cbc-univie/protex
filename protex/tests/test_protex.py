6fa85b46fcfab7590e11e07dcbdcde8a46e8e00b
Lint Action <lint-action@samuelmeuli.com>
2022-09-13 08:41:54 +0000

    Fix code style issues with Black

--- protex/tests/test_protex.py
+++ protex/tests/test_protex.py
@@ -7,6 +7,7 @@ import protex
 import pytest
 import sys
 
+
 def test_protex_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "protex" in sys.modules
