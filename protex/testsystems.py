a842efe7f30544b66e29b5343ee9f38c4d02f8e4
godenymarta <godeny.marta@gmail.com>
2023-02-08 14:29:57 +0100

    updated generate_hpts_meoh with most actual toppar files, created generate for 0.4 LJ scaling

--- protex/testsystems.py
+++ protex/testsystems.py
@@ -941,13 +941,216 @@ def generate_hpts_meoh_system(
             params = CharmmParameterSet(*para_files)
         else:
             PARA_FILES = [
-                "toppar_drude_master_protein_2013f_lj025_modhpts.str",
+                "toppar_drude_master_protein_2013f_lj025_modhpts_chelpg.str",
                 "hoac_d.str",
                 "im1h_d.str",
                 "im1_dummy_d.str",
                 "oac_dummy_d.str",
-                "hpts_dummy_d.str",
-                "hptsh_d.str",
+                "hpts_dummy_d_chelpg.str",
+                "hptsh_d_chelpg.str",
+                "meoh_dummy.str",
+                "meoh2_unscaled.str",
+            ]
+            base = f"{protex.__path__[0]}/forcefield"  # NOTE: this points now to the installed files!
+            params = CharmmParameterSet(
+                *[f"{base}/toppar/{para_files}" for para_files in PARA_FILES]
+            )
+        if psf_file is None:
+            psf_file = f"{base}/hpts.psf"
+        psf = CharmmPsfFile(psf_file)
+        xtl = boxl * angstroms
+        psf.setBox(xtl, xtl, xtl)
+        # cooridnates can be provieded by CharmmCrdFile, CharmmRstFile or PDBFile classes
+        if crd_file is None:
+            crd_file = f"{base}/hpts.crd"
+        crd = CharmmCrdFile(crd_file)
+        return psf, crd, params
+
+    def setup_system(constraints=constraints):
+        psf, crd, params = load_charmm_files()
+        params.atom_types_str["DUMH"].set_lj_params(
+            -0.00001,
+            params.atom_types_str["DUMH"].rmin,
+            -0.00001,
+            params.atom_types_str["DUMH"].rmin_14,
+        )
+
+        if constraints is None:
+            system = psf.createSystem(
+                params,
+                nonbondedMethod=PME,
+                nonbondedCutoff=11.0 * angstroms,
+                switchDistance=10 * angstroms,
+                constraints=None,
+            )
+        elif constraints == "HBonds":
+            system = psf.createSystem(
+                params,
+                nonbondedMethod=PME,
+                nonbondedCutoff=11.0 * angstroms,
+                switchDistance=10 * angstroms,
+                constraints=HBonds,
+            )
+        else:
+            print(
+                "Only contraints=None or constraints=HBonds (given as string in function call) implemented"
+            )
+        return system
+
+    def setup_simulation(
+        restart_file=restart_file, coll_freq=coll_freq, drude_coll_freq=drude_coll_freq
+    ):
+        psf, crd, params = load_charmm_files()
+        system = setup_system()
+
+        # coll_freq = 10
+        # drude_coll_freq = 80
+
+        try:
+            # plugin
+            # https://github.com/z-gong/openmm-velocityVerlet
+            from velocityverletplugin import VVIntegrator
+
+            # temperature grouped nose hoover thermostat
+            integrator = VVIntegrator(
+                300 * kelvin,
+                coll_freq / picoseconds,
+                1 * kelvin,
+                drude_coll_freq / picoseconds,
+                0.0005 * picoseconds,
+            )
+            # test if platform and integrator are compatible -> VVIntegrator only works on cuda
+            # If we do not create a context it is not tested if there is cuda availabe for the plugin
+            context = Context(system, integrator)
+            del context
+            # afterwards the integrator is already bound to the context and we need a new one...
+            # is there something like integrator.reset()?
+            integrator = VVIntegrator(
+                300 * kelvin,
+                coll_freq / picoseconds,
+                1 * kelvin,
+                drude_coll_freq / picoseconds,
+                0.0005 * picoseconds,
+            )
+
+        except (ModuleNotFoundError, OpenMMException):
+            integrator = DrudeNoseHooverIntegrator(
+                300 * kelvin,
+                coll_freq / picoseconds,
+                1 * kelvin,
+                drude_coll_freq / picoseconds,
+                0.0005 * picoseconds,
+            )
+
+        print(
+            f"{coll_freq=}, {drude_coll_freq=}"
+        )  # tested with 20, 40, 80, 100, 120, 140, 160: 20,40 bad; 80 - 120 good; 140, 160 crashed
+        integrator.setMaxDrudeDistance(0.25 * angstroms)
+        try:
+            platform = Platform.getPlatformByName("CUDA")
+            prop = dict(CudaPrecision="single")  # default is single
+            # Moved creating the simulation object inside the try...except block, because i.e.
+            # Error loading CUDA module: CUDA_ERROR_INVALID_PTX (218)
+            # message was only thrown during simulation creation not by specifying the platform
+            simulation = Simulation(
+                psf.topology,
+                system,
+                integrator,
+                platform=platform,
+                platformProperties=prop,
+            )
+        except OpenMMException:
+            platform = Platform.getPlatformByName("CPU")
+            prop = dict()
+            simulation = Simulation(
+                psf.topology,
+                system,
+                integrator,
+                platform=platform,
+                platformProperties=prop,
+            )
+
+        simulation.context.setPositions(crd.positions)
+        # Try with positions from equilibrated system:
+        # if restart_file is None:
+        #     base = f"{protex.__path__[0]}/forcefield"
+        #     restart_file = f"{base}/traj/hpts_npt_7.rst"
+        if restart_file is not None:
+            with open(restart_file) as f:
+                print(f"Opening restart file {f}")
+                simulation.context.setState(XmlSerializer.deserialize(f.read()))
+            simulation.context.computeVirtualSites()
+        else:
+            print("No restart file found. Using initial coordinate file.")
+            simulation.context.computeVirtualSites()
+            simulation.context.setVelocitiesToTemperature(300 * kelvin)
+
+        # set nonbonded parameters including dummy zero again.
+        # had to be  zero durin creating becuase of:
+        # openmm.OpenMMException: updateParametersInContext: The set of non-excluded exceptions has changed
+        nonbonded_force = [
+            f for f in simulation.system.getForces() if isinstance(f, mm.NonbondedForce)
+        ][0]
+        dummy_atoms = []
+        for atom in simulation.topology.atoms():
+            if atom.residue.name == "IM1" and atom.name == "H7":
+                dummy_atoms.append(atom.index)
+                nonbonded_force.setParticleParameters(atom.index, 0.0, 0.0, 0.0)
+            if atom.residue.name == "OAC" and atom.name == "H":
+                dummy_atoms.append(atom.index)
+                nonbonded_force.setParticleParameters(atom.index, 0.0, 0.0, 0.0)
+            if atom.residue.name == "HPTS" and atom.name == "H7":
+                dummy_atoms.append(atom.index)
+                nonbonded_force.setParticleParameters(atom.index, 0.0, 0.0, 0.0)
+            if atom.residue.name == "MEOH" and atom.name == "HO2":
+                dummy_atoms.append(atom.index)
+                nonbonded_force.setParticleParameters(atom.index, 0.0, 0.0, 0.0)
+        for exc_id in range(nonbonded_force.getNumExceptions()):
+            f = nonbonded_force.getExceptionParameters(exc_id)
+            idx1 = f[0]
+            idx2 = f[1]
+            chargeProd, sigma, epsilon = f[2:]
+            if idx1 in dummy_atoms or idx2 in dummy_atoms:
+                nonbonded_force.setExceptionParameters(
+                    exc_id, idx1, idx2, 0.0, sigma, 0.0
+                )
+
+        return simulation
+
+    return setup_simulation()
+
+def generate_hpts_meoh_lj04_system(
+    psf_file: str = None,
+    crd_file: str = None,
+    restart_file: str = None,
+    constraints: str = None,
+    boxl: float = 70.0,
+    para_files: list[str] = None,
+    coll_freq=10,
+    drude_coll_freq=100,
+):
+    """Sets up a solvated and paraterized system for IM1H/OAC/HPTS/MEOH."""
+
+    def load_charmm_files(
+        psf_file=psf_file, crd_file=crd_file, boxl=boxl, para_files=para_files
+    ):
+        # =======================================================================
+        # Force field
+        # =======================================================================
+        # Loading CHARMM files
+        print("Loading CHARMM files...")
+        if para_files is not None:
+            print("Using user supplied parameter files. Whole path must be given")
+            params = CharmmParameterSet(*para_files)
+        else:
+            PARA_FILES = [
+                "toppar_drude_master_protein_2013f_lj04_modhpts_chelpg.str",
+                "hoac_d.str",
+                "im1h_d.str",
+                "im1_dummy_d.str",
+                "oac_dummy_d.str",
+                "hpts_dummy_d_chelpg.str",
+                "hptsh_d_chelpg.str",
                 "meoh_dummy.str",
                 "meoh2_unscaled.str",
             ]
