992ae197b7b9164d7e88987ee065a5aa6af4b171
florianj77 <florian.joerg@aon.at>
2023-02-08 11:02:11 +0100

    residue file

--- protex/tests/test_residue.py
+++ protex/tests/test_residue.py
@@ -1,20 +1,59 @@
 import logging
+import os
 from collections import defaultdict
+from sys import stdout
 
+import pytest
 
+import protex
 
 try:  # Syntax changed in OpenMM 7.6
     import openmm as mm
-    from openmm.unit import angstroms, md_kilocalories
+    from openmm import (
+        Context,
+        DrudeNoseHooverIntegrator,
+        OpenMMException,
+        Platform,
+        XmlSerializer,
+    )
+    from openmm.app import (
+        PME,
+        CharmmCrdFile,
+        CharmmParameterSet,
+        CharmmPsfFile,
+        DCDReporter,
+        HBonds,
+        PDBReporter,
+        Simulation,
+        StateDataReporter,
+    )
+    from openmm.unit import angstroms, kelvin, md_kilocalories, nanometers, picoseconds
 except ImportError:
     import simtk.openmm as mm
-    from simtk.unit import angstroms
+    from simtk.openmm import (
+        OpenMMException,
+        Platform,
+        Context,
+        DrudeNoseHooverIntegrator,
+        XmlSerializer,
+    )
+    from simtk.openmm.app import DCDReporter, PDBReporter, StateDataReporter
+    from simtk.openmm.app import CharmmCrdFile, CharmmParameterSet, CharmmPsfFile
+    from simtk.openmm.app import PME, HBonds
+    from simtk.openmm.app import Simulation
+    from simtk.unit import angstroms, kelvin, picoseconds, nanometers
 
+from ..reporter import ChargeReporter, EnergyReporter
+from ..residue import Residue
 from ..system import ProtexSystem, ProtexTemplates
 from ..testsystems import (
     IM1H_IM1,
     OAC_HOAC,
+    generate_im1h_oac_dummy_system,
+    generate_im1h_oac_system,
+    generate_im1h_oac_system_clap,
     generate_single_im1h_oac_system,
+    generate_small_box,
 )
 from ..update import NaiveMCUpdate, StateUpdate
 
@@ -44,6 +83,32 @@ def test_update_single():
         "NonbondedForce",
     ]
     LOGGER.debug(f"{FORCES=}")
+    name_function = {
+        "HarmonicBondForce": [
+            Residue._get_HarmonicBondForce_parameters_at_lambda,
+            Residue._set_HarmonicBondForce_parameters,
+        ],
+        "HarmonicAngleForce": [
+            Residue._get_HarmonicAngleForce_parameters_at_lambda,
+            Residue._set_HarmonicAngleForce_parameters,
+        ],
+        "PeriodicTorsionForce": [
+            Residue._get_PeriodicTorsionForce_parameters_at_lambda,
+            Residue._set_PeriodicTorsionForce_parameters,
+        ],
+        "CustomTorsionForce": [
+            Residue._get_CustomTorsionForce_parameters_at_lambda,
+            Residue._set_CustomTorsionForce_parameters,
+        ],
+        "DrudeForce": [
+            Residue._get_DrudeForce_parameters_at_lambda,
+            Residue._set_DrudeForce_parameters,
+        ],
+        "NonbondedForce": [
+            Residue._get_NonbondedForce_parameters_at_lambda,
+            Residue._set_DrudeForce_parameters,
+        ],
+    }
 
     pair_12_13_list = ionic_liquid._build_exclusion_list(ionic_liquid.topology)
 
@@ -148,7 +213,7 @@ def test_update_single():
             if force_name in FORCES:
                 LOGGER.debug(f"At force {force_name}")
                 atom_idxs = residue.atom_idxs
-                get_params(force, force_name, atom_idxs, forces_dict0)
+                params0 = get_params(force, force_name, atom_idxs, forces_dict0)
 
         assert forces_dict0 == forces_orig
 
@@ -157,7 +222,7 @@ def test_update_single():
             if force_name in FORCES:
                 residue.update(force_name, 1)
                 ionic_liquid.update_context(force_name)
-                get_params(force, force_name, atom_idxs, forces_dict1)
+                params2 = get_params(force, force_name, atom_idxs, forces_dict1)
         # assert forces_dict1 == forces_alternativ # npt working because indices change
         for key in forces_dict1:
             # after update 1 -> changes
