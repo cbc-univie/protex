de5a366018d9b23fdb8ae9d9a557a5dcdc12401b
florianj77 <florian.joerg@aon.at>
2023-05-17 15:01:54 +0200

    example to generate a psf file without charmm

---
+++ templates/im1h_oac/generate_psf.py
@@ -0,0 +1,29 @@
+import sys
+#pip install git+https://github.com/florianjoerg/cbchelpers.git
+from cbchelpers.parmed_extension import enable_psf_from_scratch
+enable_psf_from_scratch()
+from parmed.charmm import CharmmParameterSet, CharmmPsfFile
+
+def main():
+    para_files = [
+        "../../protex/forcefield/toppar/toppar_drude_master_protein_2013f_lj025.str",
+        "../../protex/forcefield/toppar/im1h_d.str",
+        "../../protex/forcefield/toppar/oac_d_dummy.str",
+        "../../protex/forcefield/toppar/im1_d_dummy.str",
+        "../../protex/forcefield/toppar/hoac_d.str",
+    ]
+
+    molecules = {
+        "IM1H": {"number": 150, "drude_mass": 0.4},
+        "OAC": {"number": 150, "drude_mass": 0.4},
+        "IM1": {"number": 350, "drude_mass": 0.4},
+        "HOAC": {"number": 350, "drude_mass": 0.4},
+    }
+
+    params = CharmmParameterSet(*para_files)
+    psf = CharmmPsfFile.from_scratch(params, molecules)
+    print(len(psf.residues))
+    psf.write_psf("im1h_oac_150_im1_hoac_350.psf")
+
+if __name__ == "__main__":
+    main()
