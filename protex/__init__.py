f911c3f9f9517bb77488acd18ccbab9dbed6c523
florianj77 <florian.joerg@aon.at>
2023-05-24 13:51:31 +0200

    add custom ProtexException

--- protex/__init__.py
+++ protex/__init__.py
@@ -3,15 +3,15 @@ Proton exchange using SAMS and openMM for ionic liquids.
 """
 
 # Handle versioneer
-#from ._version import get_versions
+# from ._version import get_versions
 
 # Add imports here
 from .protex import *
 
-#versions = get_versions()
-#__version__ = versions["version"]
-#__git_revision__ = versions["full-revisionid"]
-#del get_versions, versions
+# versions = get_versions()
+# __version__ = versions["version"]
+# __git_revision__ = versions["full-revisionid"]
+# del get_versions, versions
 try:
     from ._version import version as __version__
     from ._version import version_tuple
@@ -25,3 +25,7 @@ import logging
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)1s()] %(message)s"
 # set logging level
 logging.basicConfig(format=FORMAT, datefmt="%d-%m-%Y:%H:%M", level=logging.INFO)
+
+
+class ProtexException(Exception):
+    pass
